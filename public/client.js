4d25575415054d1f0c197e280e409c4c50d67d8e
tamastatai <tataitamas@gmail.com>
2017-01-11 12:28:16 +0100

    client, server improved

--- public/client.js
+++ public/client.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const Feedback = (function () {
+const Feedback = function () {
   const button = document.querySelector('button');
   const textarea = document.querySelector('textarea');
   const number = document.querySelector('.number');
@@ -9,23 +9,28 @@ const Feedback = (function () {
   let send = function () {
     ajax.postMessage(textarea.value, number.value, email.value);
   };
+
   button.addEventListener('click', send);
 
   return {
     send: send
-  }
-
-}) ();
+  };
 
 const changeContents = function() {
   const list = document.querySelector('ul');
   const section = document.querySelector('section');
   const span = document.querySelector('span');
 
-  let renderResponse = function (data) {
-    data.projects.forEach(function(item){
-      createListItem(item);
+  let renderResponse = function (items) {
+    showContent();
+    items.forEach(function(items){
+      createListItem(items);
     })
+  };
+
+  let renderError = function () {
+    span.classList.remove('hide');
+    span.innerHTML = 'Hasta la vista!'
   };
 
   let createListItem = function (text) {
@@ -46,35 +51,38 @@ const changeContents = function() {
 
   return {
     renderResponse: renderResponse,
+    renderError: renderError,
+    createListItem: createListItem,
     displayLoading: displayLoading,
     showContent: showContent
   };
 
-}) ();
-
 const ajax = function () {
-
+  let data = [];
   let postMessage = function (text, number, email) {
-    console.log(text + ' ' + number + ' ' + email);
+    changeContents.displayLoading();
 
-    let xhr = new XMLHttpRequest();
-
+  let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
       if (xhr.readyState == XMLHttpRequest.DONE) {
-        JSON.parse(xhr.response);
+        console.log(xhr.response);
+        data = JSON.parse(xhr.response);
+        // console.log(data);
+        if (data.status === 'OK') {
+          changeContents.renderResponse(data.items)
+        } else {
+          changeContents.renderError();
+        }
       };
     };
 
     xhr.open('POST', 'http://localhost:3000/exam', true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send(JSON.stringify({"feedback": text, "scale": number, "email": email}));
-    console.log({"feedback": text, "scale": number, "email": email});
-
+    // console.log({"feedback": text, "scale": number, "email": email});
+    // console.log(data);
   }
 
   return {
     postMessage: postMessage
-  }
-
-
-}) ();
+  };
--- server.js
+++ server.js
@@ -21,6 +21,22 @@ connection.connect(function connectMsql(error) {
   }
 });
 
+var praise = ["amazing", "awesome", "blithesome", "excellent", "fabulous", "fantastic", "favorable", "fortuitous", "great", "incredible", "ineffable", "mirthful", "outstanding", "perfect", "propitious", "remarkable", "smart", "spectacular", "splendid", "stellar", "stupendous", "super", "ultimate", "unbelievable", "wondrous"];
+
+var validator = function (req) {
+  let text = req.body.feedback;
+  let scale = parseInt(req.body.scale);
+  let email = req.body.email;
+
+  if (email.indexOf != -1  && scale >= 10) {
+    return true;
+  } else {
+    return false;
+  };
+};
+
+module.exports = validator;
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('client'));
@@ -32,7 +48,32 @@ app.use(function use(req, res, next) {
   next();
 });
 
+app.post('/exam', function(req, res) {
+  console.log(req.body);
+  console.log(validator(req));
+  if (validator(req) == true) {
+    connection.query({
+      sql: 'SELECT project_name FROM projects'},
+      function sendBackProjects(err, rows) {
+        console.log(rows);
+      if (err) {
+        console.log(err.toString());
+        return;
+      }
+      let content = rows.map(function () {
+        return e.project_name;
+      });
+      console.log("yeah");
+      res.send({"status": "ok", "projects": content});
+      console.log({"status": "ok", "projects": content});
+    });
+  } else {
+    res.send({"status": "error", "message": "thank you"});
+  }
+
+});
+
 var port = process.env.PORT || 3001;
 app.listen(port, function () {
-  console.log('Server running on port %d', port);
+  console.log('Server is killing it on port %d', port);
 });
